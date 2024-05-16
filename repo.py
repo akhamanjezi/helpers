9c20180f2ae0c146ab0673c8a1ca7eaf953a0c85
akhamanjezi <amanjezi@gmail.com>
2024-05-16 23:37:38 +0200

    Create repo with font dict

---
+++ repo.py
@@ -0,0 +1,13 @@
+FONT = {
+    'Large Title': {'Weight': 'Regular', 'Size (points)': 34, 'Leading (points)': 41},
+    'Title 1': {'Weight': 'Regular', 'Size (points)': 28, 'Leading (points)': 34},
+    'Title 2': {'Weight': 'Regular', 'Size (points)': 22, 'Leading (points)': 28},
+    'Title 3': {'Weight': 'Regular', 'Size (points)': 20, 'Leading (points)': 25},
+    'Headline': {'Weight': 'Semibold', 'Size (points)': 17, 'Leading (points)': 22},
+    'Body': {'Weight': 'Regular', 'Size (points)': 17, 'Leading (points)': 22},
+    'Callout': {'Weight': 'Regular', 'Size (points)': 16, 'Leading (points)': 21},
+    'Subhead': {'Weight': 'Regular', 'Size (points)': 15, 'Leading (points)': 20},
+    'Footnote': {'Weight': 'Regular', 'Size (points)': 13, 'Leading (points)': 18},
+    'Caption 1': {'Weight': 'Regular', 'Size (points)': 12, 'Leading (points)': 16},
+    'Caption 2': {'Weight': 'Regular', 'Size (points)': 11, 'Leading (points)': 13}
+}
